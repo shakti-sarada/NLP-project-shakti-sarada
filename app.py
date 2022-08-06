c5e6a41669156ebaf696645f50528381db8d5ec4
shakti-sarada <83201466+shakti-sarada@users.noreply.github.com>
2022-08-06 20:20:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open('naivebayes_modelNLP1.pkl','rb')) 
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+
+def predict():
+  dataset = pd.read_csv('NLP dataset 1.csv')
+  # First step: cleaning Text and removing number and punctuation marks.
+  # Cleaning the texts for all review using for loop
+  import re
+  import nltk
+  nltk.download('stopwords')
+  from nltk.corpus import stopwords
+  from nltk.stem.porter import PorterStemmer
+  corpus = []
+  for i in range(0, 479):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['text'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    #print(review)
+    corpus.append(review)
+  # Creating the Bag of Words model
+  from sklearn.feature_extraction.text import CountVectorizer
+  cv = CountVectorizer(max_features = 1500)
+  #print(cv)
+  X = cv.fit_transform(corpus).toarray()
+  import re
+  review = re.sub('[^a-zA-Z]', ' ', str('text'))
+  review=review.lower()
+  print(review)
+  # Third step: Removing stop words like 'this, the'
+  import nltk
+  nltk.download('stopwords')
+  from nltk.corpus import stopwords
+  review = review.split()
+  print(review)
+  # Third step: Removing stop words like 'this, the'
+   # set function is generally used for long article to fastem process
+  review1 = [word for word in review if not word in set(stopwords.words('english'))]
+  print(review1)
+  # Fourth step: converting stemming words
+  from nltk.stem.porter import PorterStemmer
+  ps = PorterStemmer()
+  review = [ps.stem(word) for word in review1 if not word in set(stopwords.words('english'))]
+  print(review)
+  # joining these words of list
+  review2 = ' '.join(review)
+  print(review2)
+  # Creating the Bag of Words model
+  
+  X = cv.transform(review).toarray()
+  input_pred = model.predict(X)
+  input_pred = input_pred.astype(int)
+  print(input_pred)
+  if input_pred[0]==1:
+    result= "Review is Positive"
+  else:
+    result="Review is negative" 
+    
+    
+  return render_template('index.html', prediction_text='Review Anlaysis: {}'.format(result))
+ 
+app.run()
